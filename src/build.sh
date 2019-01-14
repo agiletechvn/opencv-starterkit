70421d36df883a615245dab8c7d019ab8d8cb3d4
tu@agiletech.vn <tubackkhoa@gmail.com>
2019-01-14 13:23:53 +0700

    allow extra arguments to build g++ like -O3, -Ofast

--- src/build.sh
+++
@@ -1,6 +0,0 @@
-INPUT=$1
-# remove extension
-OUTPUT=${INPUT%%.*}
-# build
-echo 'g++ -std=c++0x `pkg-config --cflags opencv` '$INPUT' `pkg-config --libs opencv` -o '$OUTPUT
-g++ -std=c++0x `pkg-config --cflags opencv` $INPUT `pkg-config --libs opencv` -o $OUTPUT
