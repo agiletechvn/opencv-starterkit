c9d5b5c497bd9b0c8f195b4169f53223c9a70a52
tu@agiletech.vn <tubackkhoa@gmail.com>
2019-01-15 17:53:30 +0700

    need testing arguments

--- src/Calibration/videoRead.py
+++ src/Calibration/videoRead.py
@@ -12,10 +12,10 @@ assert float(cv2.__version__.rsplit('.', 1)[
 
 
 # Initialize the parameters
-confThreshold = 0.5  # Confidence threshold
-nmsThreshold = 0.3  # Non-maximum suppression threshold
-inpWidth = 416  # Width of network's input image
-inpHeight = 416  # Height of network's input image
+confThreshold = 0.0  # Confidence threshold
+nmsThreshold = 0.0  # Non-maximum suppression threshold
+inpWidth = 608  # Width of network's input image
+inpHeight = 608  # Height of network's input image
 
 
 # Load names of classes
@@ -33,6 +33,36 @@ net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
 net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
 
+def getTracker(tracker_index=2):
+
+    # Set up tracker.
+    # Instead of MIL, you can also use
+
+    tracker_types = ['BOOSTING', 'MIL', 'KCF',
+                     'TLD', 'MEDIANFLOW', 'GOTURN', 'CSRT']
+    tracker_type = tracker_types[tracker_index]
+
+    if int(minor_ver) < 3:
+        tracker = cv2.Tracker_create(tracker_type)
+    else:
+        if tracker_type == 'BOOSTING':
+            tracker = cv2.TrackerBoosting_create()
+        if tracker_type == 'MIL':
+            tracker = cv2.TrackerMIL_create()
+        if tracker_type == 'KCF':
+            tracker = cv2.TrackerKCF_create()
+        if tracker_type == 'TLD':
+            tracker = cv2.TrackerTLD_create()
+        if tracker_type == 'MEDIANFLOW':
+            tracker = cv2.TrackerMedianFlow_create()
+        if tracker_type == 'GOTURN':
+            tracker = cv2.TrackerGOTURN_create()
+        if tracker_type == "CSRT":
+            tracker = cv2.TrackerCSRT_create()
+
+    return tracker
+
+
 # Get the names of the output layers
 def getOutputsNames(net):
     # Get the names of all the layers in the network
@@ -44,7 +74,7 @@ def getOutputsNames(net):
 # Draw the predicted bounding box
 def drawPred(frame, classId, conf, left, top, right, bottom):
     # Draw a bounding box.
-    cv2.rectangle(frame, (left, top), (right, bottom), (255, 178, 50), 3)
+    cv2.rectangle(frame, (left, top), (right, bottom), (255, 178, 50), 2)
 
     label = '%.2f' % conf
 
@@ -55,12 +85,12 @@ def drawPred(frame, classId, conf, left, top, right, bottom):
 
     # Display the label at the top of the bounding box
     labelSize, baseLine = cv2.getTextSize(
-        label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+        label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 1)
     top = max(top, labelSize[1])
-    cv2.rectangle(frame, (left, top - round(1.5*labelSize[1])), (left + round(
-        1.5*labelSize[0]), top + baseLine), (255, 255, 255), cv2.FILLED)
+    cv2.rectangle(frame, (left, top - round(1.1*labelSize[1])), (left + round(
+        1.1*labelSize[0]), top + baseLine), (255, 255, 255), cv2.FILLED)
     cv2.putText(frame, label, (left, top),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 0), 1)
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
 
 
 # Remove the bounding boxes with low confidence using non-maxima suppression
@@ -68,9 +98,6 @@ def postprocess(frame, outs):
     frameHeight = frame.shape[0]
     frameWidth = frame.shape[1]
 
-    classIds = []
-    confidences = []
-    boxes = []
     # Scan through all the bounding boxes output from the network and keep only the
     # ones with high confidence scores. Assign the box's class label as the class with the highest score.
     classIds = []
@@ -105,6 +132,8 @@ def postprocess(frame, outs):
         drawPred(frame, classIds[i], confidences[i], left,
                  top, left + width, top + height)
 
+    return boxes
+
 
 @click.command()
 @click.option('--videopath', default='fisheye.3gp',
@@ -123,7 +152,7 @@ def main(videopath, start):
 
     cv2.namedWindow('fisheye', cv2.WINDOW_NORMAL)
 
-    scale = 0.33
+    scale = 0.4
 
     # get cap property
     cap.set(cv2.CAP_PROP_POS_MSEC, start * 1000)
@@ -147,20 +176,53 @@ def main(videopath, start):
     Knew = K.copy()
     # Knew[(0, 1), (0, 1)] = 1.1 * Knew[(0, 1), (0, 1)]
 
+    boxes = []
+    trackers = []
+
     # Read until video is completed
     while(cap.isOpened()):
         # Capture frame-by-frame
         ret, frame = cap.read()
         if ret == True:
             dst_frame = cv2.resize(frame, None, fx=scale, fy=scale)
-            # frame_cropped = frame[y:y+h, x:x+w]
+
             # frame_undistorted = dst_frame
             frame_undistorted = cv2.fisheye.undistortImage(
                 dst_frame, K, D=D, Knew=Knew)
 
+            # if len(boxes) > 0:
+            #     if len(trackers) == 0:
+            #         tracker = getTracker()
+            #         trackers.append(tracker)
+            #         # Initialize tracker with first frame and bounding box
+            #         ok = tracker.init(frame_undistorted, tuple(boxes[0]))
+
+            #     # Start timer
+            #     timer = cv2.getTickCount()
+
+            #     # Update tracker
+            #     ok, bbox = trackers[0].update(frame_undistorted)
+
+            #     # Calculate Frames per second (FPS)
+            #     fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+
+            #     # Draw bounding box
+            #     if ok:
+            #         # Tracking success
+            #         p1 = (int(bbox[0]), int(bbox[1]))
+            #         p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            #         cv2.rectangle(frame_undistorted, p1, p2, (255, 0, 0), 2, 1)
+            #     else:
+            #         # Tracking failure
+            #         cv2.putText(frame_undistorted, "Tracking failure detected", (100, 80),
+            #                     cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+
+            # else:
             # Create a 4D blob from a frame.
+            inpWidth = round(width)
+            inpHeight = round(height)
             blob = cv2.dnn.blobFromImage(
-                frame_undistorted, 1/255, (inpWidth, inpHeight), [0, 0, 0], 1, crop=False)
+                frame_undistorted, 3/255, (inpWidth, inpHeight), [0, 0, 0], 1, crop=False)
             # blob = cv2.dnn.blobFromImage(
             #     frame_undistorted, swapRB=True, crop=False)
             # Sets the input to the network
@@ -170,14 +232,14 @@ def main(videopath, start):
             outs = net.forward(getOutputsNames(net))
 
             # Remove the bounding boxes with low confidence
-            postprocess(frame_undistorted, outs)
+            boxes = postprocess(frame_undistorted, outs)
 
-            # Put efficiency information. The function getPerfProfile returns the overall time for inference(t) and the timings for each of the layers(in layersTimes)
-            t, _ = net.getPerfProfile()
-            label = 'Inference time: %.2f ms' % (
-                t * 1000.0 / cv2.getTickFrequency())
-            cv2.putText(frame_undistorted, label, (0, 15),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
+            # # Put efficiency information. The function getPerfProfile returns the overall time for inference(t) and the timings for each of the layers(in layersTimes)
+            # t, _ = net.getPerfProfile()
+            # label = 'Inference time: %.2f ms' % (
+            #     t * 1000.0 / cv2.getTickFrequency())
+            # cv2.putText(frame_undistorted, label, (0, 10),
+            #             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255))
 
             # Display the resulting frame
             cv2.imshow('fisheye', frame_undistorted)
