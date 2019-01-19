a857961c2c94d06acae8bdbfa5f819fb47b97361
tu@agiletech.vn <tubackkhoa@gmail.com>
2019-01-19 12:10:22 +0700

    Background substraction

--- src/VideoReadWriteDisplay/videoRead.cpp
+++ src/VideoReadWriteDisplay/videoRead.cpp
@@ -4,42 +4,50 @@
 using namespace std;
 using namespace cv;
 
-int main(){
+int main()
+{
 
   // Create a VideoCapture object and open the input file
   // If the input is the web camera, pass 0 instead of the video file name
-  VideoCapture cap("chaplin.mp4"); 
-   
+  VideoCapture cap("chaplin.mp4");
+  // Ptr<BackgroundSubtractor> model = createBackgroundSubtractorKNN();
   // Check if camera opened successfully
-  if(!cap.isOpened()){
+  if (!cap.isOpened())
+  {
     cout << "Error opening video stream or file" << endl;
     return -1;
   }
-	
-  while(1){
+  // Mat foregroundMask, foreground;
+  while (1)
+  {
 
     Mat frame;
     // Capture frame-by-frame
     cap >> frame;
- 
+
     // If the frame is empty, break immediately
     if (frame.empty())
       break;
 
+    // model->apply(frame, foregroundMask, 0);
+
+    // foreground = Scalar::all(0);
+    // frame.copyTo(foreground, foregroundMask);
+
     // Display the resulting frame
-    imshow( "Frame", frame );
+    imshow("Frame", frame);
 
     // Press  ESC on keyboard to exit
-    char c=(char)waitKey(25);
-    if(c==27)
+    char c = (char)waitKey(25);
+    if (c == 27)
       break;
   }
- 
+
   // When everything done, release the video capture object
   cap.release();
 
   // Closes all the frames
   destroyAllWindows();
-	
+
   return 0;
 }
--- src/VideoReadWriteDisplay/videoWrite.cpp
+++ src/VideoReadWriteDisplay/videoWrite.cpp
@@ -4,44 +4,47 @@
 using namespace std;
 using namespace cv;
 
-int main(){
+int main()
+{
 
   // Create a VideoCapture object and use camera to capture the video
-  VideoCapture cap(0); 
+  VideoCapture cap(0);
 
   // Check if camera opened successfully
-  if(!cap.isOpened()){
-   	cout << "Error opening video stream" << endl;
-        return -1;
+  if (!cap.isOpened())
+  {
+    cout << "Error opening video stream" << endl;
+    return -1;
   }
-  
+
   // Default resolutions of the frame are obtained.The default resolutions are system dependent.
   int frame_width = cap.get(CV_CAP_PROP_FRAME_WIDTH);
   int frame_height = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
-  
-  // Define the codec and create VideoWriter object.The output is stored in 'outcpp.avi' file.
-  VideoWriter video("outcpp.avi",CV_FOURCC('M','J','P','G'),10, Size(frame_width,frame_height));
 
-  while(1){
+  // Define the codec and create VideoWriter object.The output is stored in 'outcpp.avi' file.
+  VideoWriter video("outcpp.avi", CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(frame_width, frame_height));
+
+  while (1)
+  {
 
     Mat frame;
-   
+
     // Capture frame-by-frame
     cap >> frame;
- 
+
     // If the frame is empty, break immediately
     if (frame.empty())
       break;
-    
+
     // Write the frame into the file 'outcpp.avi'
     video.write(frame);
-   
-    // Display the resulting frame    
-    imshow( "Frame", frame );
- 
+
+    // Display the resulting frame
+    imshow("Frame", frame);
+
     // Press  ESC on keyboard to  exit
     char c = (char)waitKey(1);
-    if( c == 27 ) 
+    if (c == 27)
       break;
   }
 
